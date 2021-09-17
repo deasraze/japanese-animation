f63272f82e5d36a77380187ae44d4c551bfb4264
deasraze <condecrom@gmail.com>
2021-09-17 10:03:16 +0300

    Added change role command

---
+++ api/src/Auth/Command/ChangeRole/Handler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\ChangeRole;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\Role;
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+
+class Handler
+{
+    public function __construct(private UserRepository $users, private Flusher $flusher)
+    {
+    }
+
+    public function handle(Command $command): void
+    {
+        $user = $this->users->get(new Id($command->id));
+
+        $user->changeRole(new Role($command->role));
+
+        $this->flusher->flush();
+    }
+}
