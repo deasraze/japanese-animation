493c464025eb55bdffd42c21497645d2cc2d8c7f
deasraze <condecrom@gmail.com>
2021-09-21 11:50:12 +0300

    Fixed code style

--- api/src/Auth/Entity/User/Id.php
+++ api/src/Auth/Entity/User/Id.php
@@ -18,17 +18,17 @@ class Id
         $this->value = mb_strtolower($value);
     }
 
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
     public static function generate(): self
     {
         return new self(Uuid::uuid4()->toString());
     }
 
     public function getValue(): string
-    {
-        return $this->value;
-    }
-
-    public function __toString(): string
     {
         return $this->value;
     }
