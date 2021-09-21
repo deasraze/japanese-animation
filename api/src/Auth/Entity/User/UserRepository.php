7d7c6a34dd584ac0a884f7d16a95514d0897d3ca
deasraze <condecrom@gmail.com>
2021-09-21 09:27:32 +0300

    Implemented User Repository

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -4,23 +4,86 @@ declare(strict_types=1);
 
 namespace App\Auth\Entity\User;
 
-interface UserRepository
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use DomainException;
+
+class UserRepository
 {
-    public function get(Id $id): User;
+    private EntityManagerInterface $em;
+    /**
+     * @var EntityRepository<User>
+     */
+    private EntityRepository $repo;
 
-    public function getByEmail(Email $email): User;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(User::class);
+    }
 
-    public function findByJoinConfirmToken(string $token): ?User;
+    public function get(Id $id): User
+    {
+        $user = $this->repo->find($id->getValue());
 
-    public function findByResetPasswordToken(string $token): ?User;
+        if (null === $user) {
+            throw new DomainException('User is not found.');
+        }
 
-    public function findByNewEmailToken(string $token): ?User;
+        return $user;
+    }
 
-    public function hasByEmail(Email $email): bool;
+    public function getByEmail(Email $email): User
+    {
+        $user = $this->repo->findOneBy(['email' => $email->getValue()]);
 
-    public function hasByNickname(Name $name): bool;
+        if (null === $user) {
+            throw new DomainException('User is not found.');
+        }
 
-    public function add(User $user): void;
+        return $user;
+    }
 
-    public function remove(User $user): void;
+    public function findByJoinConfirmToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+    }
+
+    public function findByResetPasswordToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['resetPasswordToken.value' => $token]);
+    }
+
+    public function findByNewEmailToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['newEmailToken.value' => $token]);
+    }
+
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->where('t.email = :email')
+            ->setParameter('email', $email->getValue())
+            ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByNickname(Name $name): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->where('t.name.nickname = :nickname')
+            ->setParameter('nickname', $name->getNickname())
+            ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+
+    public function remove(User $user): void
+    {
+        $this->em->remove($user);
+    }
 }
