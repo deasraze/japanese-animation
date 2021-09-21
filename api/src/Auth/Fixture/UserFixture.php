b3f6f436ebaedf4e004f58c0c26f2ac3e76703c9
deasraze <condecrom@gmail.com>
2021-09-21 09:39:08 +0300

    Added user fixture

---
+++ api/src/Auth/Fixture/UserFixture.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Fixture;
+
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\Name;
+use App\Auth\Entity\User\Token;
+use App\Auth\Entity\User\User;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+
+class UserFixture extends Fixture
+{
+    // password
+    private const HASH = '$2y$13$ccyFAR0s0C96hIFC2oi6N.BFOB82GA73HGG8Ij27Aro6Dn6s9NZ.O';
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = User::requestJoinByEmail(
+            new Id('00000000-0000-0000-0000-000000000001'),
+            $date = new DateTimeImmutable('-30 days'),
+            new Email('admin@app.test'),
+            new Name('Admin'),
+            self::HASH,
+            new Token($value = Uuid::uuid4()->toString(), $date->modify('+1 day'))
+        );
+
+        $user->confirmJoin($value, $date);
+
+        $manager->persist($user);
+
+        $user = User::requestJoinByEmail(
+            new Id('00000000-0000-0000-0000-000000000002'),
+            $date = new DateTimeImmutable('-28 days'),
+            new Email('user@app.test'),
+            new Name('user001'),
+            self::HASH,
+            new Token($value = Uuid::uuid4()->toString(), $date->modify('+1 day'))
+        );
+
+        $user->confirmJoin($value, $date);
+
+        $manager->persist($user);
+    }
+}
