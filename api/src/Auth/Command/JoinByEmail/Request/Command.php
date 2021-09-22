8c9f6e088e530d5993aee2f9169baa4e612bfee0
deasraze <condecrom@gmail.com>
2021-09-22 15:48:47 +0300

    Migrated to assert group sequence

--- api/src/Auth/Command/JoinByEmail/Request/Command.php
+++ api/src/Auth/Command/JoinByEmail/Request/Command.php
@@ -6,20 +6,25 @@ namespace App\Auth\Command\JoinByEmail\Request;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * @Assert\GroupSequence({"Command", "Strict"})
+ */
 class Command
 {
     /**
      * @Assert\NotBlank
-     * @Assert\Email
+     * @Assert\Email(groups={"Strict"})
      */
     public string $email = '';
     /**
-     * @Assert\Type("alnum")
-     * @Assert\Length(min=4, max=20)
+     * @Assert\NotBlank
+     * @Assert\Type("alnum", groups={"Strict"})
+     * @Assert\Length(min=4, max=20, groups={"Strict"})
      */
     public string $nickname = '';
     /**
-     * @Assert\Length(min=8)
+     * @Assert\NotBlank
+     * @Assert\Length(min=8, groups={"Strict"})
      */
     public string $password = '';
 }
