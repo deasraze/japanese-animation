f98aa3f86afece10bc79fd11187896073d73056c
deasraze <condecrom@gmail.com>
2021-10-07 11:50:19 +0300

    Added guid annotation

--- api/src/Http/Action/V1/Auth/BlockAction.php
+++ api/src/Http/Action/V1/Auth/BlockAction.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Action\V1\Auth;
 
+use App\Annotation\Guid;
 use App\Auth\Command\Block;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/auth/{id}/block', methods: ['PUT'])]
+#[Route('/auth/{id}/block', requirements: ['id' => Guid::PATTERN], methods: ['PUT'])]
 #[IsGranted('ROLE_ADMIN')]
 class BlockAction extends AbstractController
 {
