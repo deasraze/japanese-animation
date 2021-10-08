cce61a36671042b2fd79cf16acfc84a79bedef6f
deasraze <condecrom@gmail.com>
2021-10-08 16:15:28 +0300

    Added preferred locale subscriber

---
+++ api/src/Http/Request/PreferredLocaleSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Request;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class PreferredLocaleSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 16]],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if ($preferredLanguage = $event->getRequest()->getPreferredLanguage()) {
+            $event->getRequest()->setLocale($preferredLanguage);
+        }
+    }
+}
