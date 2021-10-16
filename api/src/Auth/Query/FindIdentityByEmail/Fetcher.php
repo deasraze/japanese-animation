f4aeed37f89e5c4845428e019d491043b74c6983
deasraze <condecrom@gmail.com>
2021-10-16 11:35:21 +0300

    Updated doctrine dbal & migrations

--- api/src/Auth/Query/FindIdentityByEmail/Fetcher.php
+++ api/src/Auth/Query/FindIdentityByEmail/Fetcher.php
@@ -6,7 +6,6 @@ namespace App\Auth\Query\FindIdentityByEmail;
 
 use App\Auth\Entity\User\Status;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Result;
 
 class Fetcher
 {
@@ -16,7 +15,6 @@ class Fetcher
 
     public function fetch(string $email): ?Identity
     {
-        /** @var Result $stmt */
         $stmt = $this->connection->createQueryBuilder()
             ->select([
                 'id',
@@ -28,7 +26,7 @@ class Fetcher
             ->from('auth_users')
             ->where('email = :email')
             ->setParameter('email', mb_strtolower($email))
-            ->execute();
+            ->executeQuery();
 
         /**
          * @var array{
