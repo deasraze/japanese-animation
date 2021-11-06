e3e3344471b6d80760449bb9c963be39837d4fb8
deasraze <condecrom@gmail.com>
2021-11-06 12:36:04 +0300

    Fill user id for jwt payload

---
+++ api/src/Security/Jwt/FillUserIdForPayloadSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Jwt;
+
+use App\Security\UserIdentity;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class FillUserIdForPayloadSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::JWT_CREATED => 'onJWTCreated',
+        ];
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        /** @var UserIdentity $user */
+        $user = $event->getUser();
+        $payload = $event->getData();
+
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+    }
+}
